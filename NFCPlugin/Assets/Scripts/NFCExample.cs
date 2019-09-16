fcef61603e42cea31a7a080aa187f34e0cd90d92
RJ <richard.bourdelais@mymail.champlain.edu>
2019-09-16 17:19:50 -0400

    Initial commit

---
+++ NFCPlugin/Assets/Scripts/NFCExample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NFCExample : MonoBehaviour
+{
+    public Text tag_output_text;
+    AndroidJavaClass pluginTutorialActivityJavaClass;
+
+    void Start()
+    {
+        AndroidJNI.AttachCurrentThread();
+        pluginTutorialActivityJavaClass = new AndroidJavaClass("com.twinsprite.nfcplugin.NFCPluginTest");
+    }
+
+    void Update()
+    {
+        string value = pluginTutorialActivityJavaClass.CallStatic<string>("getValue");
+        tag_output_text.text = "Value:\n" + value;
+    }
+}
